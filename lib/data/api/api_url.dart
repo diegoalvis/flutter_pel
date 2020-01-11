2d2c8fb155218802ea20cd5bc6686927f77a1da6
Diego Alvis <diegoalvispal@gmail.com>
2020-01-11 18:02:03 +0100

    Define login, vendors and authentication token api request

---
+++ lib/data/api/api_url.dart
@@ -0,0 +1,5 @@
+/// API version urls
+
+const String v1 = "/pelican/v1";
+const String v2 = "/pelican/v2";
+
---
+++ lib/data/api/trivia_remote_data_source.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:pelican/data/api/api_url.dart';
+
+class ShopperApi {
+
+  final Dio _dio;
+
+  const ShopperApi(this._dio);
+
+  /// Fetch vendors list.
+  Future<Response> getVendors() => _dio.get("$v1/vendors");
+
+  /// User login.
+  Future<Response> login(String username, String password) =>
+      _dio.post("$v1/vendors", queryParameters: {"username": username, "password": password});
+
+  /// Get authentication token
+  Future<Response> getAuthToken(String token) => _dio.get("$v1/tokens/refresh", queryParameters: {"token": token});
+
+
+}
